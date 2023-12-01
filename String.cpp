e7942249fc5a78b60414843bd35c56036ca19b18
Clement Grandidier <clement.grandidier@insa-lyon.fr>
2023-12-01 08:46:38 +0100

    add size()

--- String.cpp
+++ String.cpp
@@ -4,44 +4,48 @@
 String::String(){
   // Initialize with a default string, e.g., "Hello World"
   const char* defaultString = "Hello World";
-  size = 0;
+  len = 0;
   capacity = 100; // Initial capacity, can be adjusted based on requirements
   data = new char[capacity];
 
   // Copy the default string manually
-  while (defaultString[size] != '\0' && size < capacity - 1) {
-    data[size] = defaultString[size];
-    size++;
+  while (defaultString[len] != '\0' && len < capacity - 1) {
+    data[len] = defaultString[len];
+    len++;
   }
 
   // Add null terminator
-  data[size] = '\0';
+  data[len] = '\0';
 }
 
 // Copy constructor
 String::String(const String& other) {
-  size = other.size;
+  len = other.len;
   capacity = other.capacity;
   data = new char[capacity];
 
   // Copy the data from the other string
-  for (int i = 0; i <= size; ++i) {
+  for (int i = 0; i <= len; ++i) {
     data[i] = other.data[i];
   }
 }
 
 // Constructor with a C-style string parameter
 String::String(const char* str) {
-    size = 0;
+    len = 0;
     capacity = 100; // Initial capacity, can be adjusted based on requirements
 
     data = new char[capacity];
 
-    while (str[size] != '\0' && size < capacity - 1) {
-      data[size] = str[size];
-      size++;
+    while (str[len] != '\0' && len < capacity - 1) {
+      data[len] = str[len];
+      len++;
     }
 
     // Add null terminator
-    data[size] = '\0';
+    data[len] = '\0';
+}
+
+int String::size(){
+  return len;
 }
--- String.h
+++ String.h
@@ -2,11 +2,14 @@ class String{
 
   public:
     char* data;    // pointer to the character array
-    int size;   // current size of the string
     int capacity;// capacity of the allocated memory
     String();
     String(const String& str);
     String(const char* str);
+    int size();
+
+  protected:
+    int len;
 /*
     //Accessors
     getValue();
