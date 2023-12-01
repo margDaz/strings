b68350aa882858dfdd790b5716538733d0d2c110
Clement Grandidier <clement.grandidier@insa-lyon.fr>
2023-12-01 08:19:05 +0100

    modif test

--- string_tests.cpp
+++ string_tests.cpp
@@ -1,6 +1,18 @@
 #include "String.h"
 #include <iostream>
 
+void testDefaultConstructor();
+
+void testCopyConstructor();
+// Add more test functions for other functionalities
+
+int main() {
+    testDefaultConstructor();
+    testCopyConstructor();
+    // Call other test functions
+    return 0;
+}
+
 void testDefaultConstructor() {
     String str;
     // Add assertions to check if the default constructor initializes correctly
@@ -19,13 +31,4 @@ void testCopyConstructor() {
     String str2(str);
     std::cout << str2.size << std::endl;
     std::cout << str2.data << std::endl;
-}
-
-// Add more test functions for other functionalities
-
-int main() {
-    testDefaultConstructor();
-    testCopyConstructor();
-    // Call other test functions
-    return 0;
 }
